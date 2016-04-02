245c67edec937669b83447b2e11cd61ddd9c0be9
Petr Štechmüller <stechy12@gmail.com>
2016-04-02 11:48:41 +0200

    #64 Doplnění dokumentace do třídy *ConfigurationCVEP*

--- app/src/main/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/model/ConfigurationCVEP.java
+++ app/src/main/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/model/ConfigurationCVEP.java
@@ -64,11 +64,27 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
     // endregion
     
     // region Getters & Setters
+
+    /**
+     * Vrátí počet výstupů
+     * @return Počet výstupů
+     */
     public int getOutputCount() {
         return outputCount;
     }
 
+    /**
+     * Nastaví počet výstupů
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param outputCount Počet výstupů
+     */
     public void setOutputCount(int outputCount) {setOutputCount(outputCount, null);}
+    /**
+     * Nastaví počet výstupů
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param outputCount Počet výstupů
+     * @param onValueChanged Callback, který se zavolá po nastavení počtu výstupů
+     */
     public void setOutputCount(int outputCount, OnValueChanged onValueChanged) {
         if (this.outputCount == outputCount)
             return;
@@ -80,11 +96,26 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
             onValueChanged.changed();
     }
 
+    /**
+     * Vrátí délku pulsu
+     * @return Délka pulsu
+     */
     public int getPulsLength() {
         return pulsLength;
     }
 
+    /**
+     * Nastaví délku pulsu
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param pulsLength Dálka pulsu
+     */
     public void setPulsLength(int pulsLength) {setPulsLength(pulsLength, null);}
+    /**
+     * Nastaví délku pulsu
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param pulsLength Délka pulsu
+     * @param onValueChanged Callback, který se zavolá po nastavení délky pulsu
+     */
     public void setPulsLength(int pulsLength, OnValueChanged onValueChanged) {
         if (this.pulsLength == pulsLength)
             return;
@@ -96,11 +127,26 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
             onValueChanged.changed();
     }
 
+    /**
+     * Vrátí číslo představující bitový posun, o který liší jednotlivé patterny
+     * @return Bitový posun
+     */
     public int getPulsSkew() {
         return pulsSkew;
     }
 
+    /**
+     * Nastaví bitový posun patternu
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param pulsSkew Bitový posun
+     */
     public void setPulsSkew(int pulsSkew) {setPulsSkew(pulsSkew, null);}
+    /**
+     * Nastaví bitový posun patternu
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param pulsSkew Bitový posun
+     * @param onValueChanged Callback, který se zavolá po nastavení bitového posunu
+     */
     public void setPulsSkew(int pulsSkew, OnValueChanged onValueChanged) {
         if (this.pulsSkew == pulsSkew)
             return;
@@ -112,11 +158,26 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
             onValueChanged.changed();
     }
 
+    /**
+     * Vrátí jas všech výstupů
+     * @return Jas výstupů
+     */
     public int getBrightness() {
         return brightness;
     }
 
+    /**
+     * Nastaví jas všem výstupům. Hodnoty jsou možné z intervalu <0 - 100>
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param brightness Jas výstupů
+     */
     public void setBrightness(int brightness) {setBrightness(brightness, null);}
+    /**
+     * Nastaví jas všem výstupům. Hodnoty jsou možné z intervalu <0 - 100>
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param brightness Jas výstupů
+     * @param onValueChanged Callback, který se zavolá po nastavení jasu výstupů
+     */
     public void setBrightness(int brightness, OnValueChanged onValueChanged) {
         if (this.brightness == brightness)
             return;
@@ -127,11 +188,26 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
             onValueChanged.changed();
     }
 
+    /**
+     * Vrátí referenci na hlavní pattern podle kterého se počítají zbylé patterny
+     * @return Hlavní pattern
+     */
     public Pattern getMainPattern() {
         return mainPattern;
     }
-
+    /**
+     * Nastaví hodnotu hlavního patternu
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param value Nová hodnota patternu
+     */
     public void setMainPattern(int value) {setMainPattern(value, null);}
+
+    /**
+     * Nastaví hodnotu hlavního patternu
+     * Pokud se do parametru vloží hodnota, která je stejná jako aktuální, nic se nestane
+     * @param value Nová hodnota patternu
+     * @param onValueChanged Callback, který se zavolá po nastavení nové hodnoty patternu
+     */
     public void setMainPattern(int value, final OnValueChanged onValueChanged) {
         mainPattern.setValue(value, new OnValueChanged() {
             @Override
@@ -164,10 +240,22 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
         // endregion
 
         // region Public methods
+        /**
+         * Provede cyklický bitový posun doleva o jeden bit
+         */
         public void shiftLeft() {shiftLeft(DEF_SHIFT);}
+        /**
+         * Provede cyklický bitový posun doleva
+         * @param index O kolik se má číslo posunout. Hodnota musí být větší než 0, jinak se nic nestane
+         */
         public void shiftLeft(int index) {
             shiftLeft(index, null);
         }
+        /**
+         * Provede cyklický bitový posun doleva
+         * @param index O kolik se má číslo posunout. Hodnota musí být větší než 0, jinak se nic nestane
+         * @param onValueChanged Callback, který se zavolá po bitovém posuvu
+         */
         public void shiftLeft(int index, OnValueChanged onValueChanged) {
             if (index <= 0)
                 return;
@@ -177,11 +265,23 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
             if (onValueChanged != null)
                 onValueChanged.changed();
         }
-        
+
+        /**
+         * Provede cyklický bitový posun doprava
+         */
         public void shiftRight() {shiftRight(DEF_SHIFT);}
+        /**
+         * Provede cyklický bitový posun doprava
+         * @param index O kolik se má číslo posunout. Hodnota musí být větší než 0, jinak se nic nestane
+         */
         public void shiftRight(int index) {
             shiftRight(index, null);
         }
+        /**
+         * Provede cyklický bitový posun doprava
+         * @param index O kolik se má číslo posunout. Hodnota musí být větší než 0, jinak se nic nestane
+         * @param onValueChanged Callback, který se zavolá po bitovém posuvu
+         */
         public void shiftRight(int index, OnValueChanged onValueChanged) {
             if (index <= 0)
                 return;
@@ -194,11 +294,25 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
         // endregion
         
         // region Getters & Setters
+
+        /**
+         * Vrátí název patternu
+         * @return Název patternu
+         */
         public String getName() {
             return name;
         }
 
+        /**
+         * Nastaví název patternu
+         * @param name Nový název patternu
+         */
         public void setName(String name) {setName(name, null);}
+        /**
+         * Nastaví název patternu
+         * @param name Nový název patternu
+         * @param onValueChanged Callback, který se zavolá po nastavení nového názvu patternu
+         */
         public void setName(String name, OnValueChanged onValueChanged) {
             if (this.name.equals(name) || name.isEmpty())
                 return;
@@ -209,11 +323,25 @@ public class ConfigurationCVEP extends AItem<ConfigurationCVEP> {
                 onValueChanged.changed();
         }
 
+        /**
+         * Vrátí aktuální hodnotu patternu
+         * @return Hodnota patternu
+         */
         public int getValue() {
             return value;
         }
 
+        /**
+         * Nastaví novou hodnotu patternu
+         * @param value Nová hodnota patternu
+         */
         public void setValue(int value) {setValue(value, null);}
+
+        /**
+         * Nastaví novou hodnotu patternu
+         * @param value Nová hodnota patternu
+         * @param onValueChanged Callback, který se zavolá po nastavení nové hodnoty patternu
+         */
         public void setValue(int value, OnValueChanged onValueChanged) {
             if (this.value == value)
                 return;
