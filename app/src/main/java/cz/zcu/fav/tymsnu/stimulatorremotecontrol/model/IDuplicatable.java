4cd35f19b07bcc1ca42c71d48b275ee711071e37
Petr Štechmüller <stechy12@gmail.com>
2016-04-07 20:27:34 +0200

    Dopsání komentáře do rozhraní IDuplicatable

--- app/src/main/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/model/IDuplicatable.java
+++ app/src/main/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/model/IDuplicatable.java
@@ -2,6 +2,11 @@ package cz.zcu.fav.tymsnu.stimulatorremotecontrol.model;
 
 public interface IDuplicatable<T> {
 
+    /**
+     * Vytvoří hlubokou kopii s novým názvem objektu
+     * @param newName Nový název objektu
+     * @return Hlubokou kopii objektu
+     */
     T duplicate(String newName);
 
 }
