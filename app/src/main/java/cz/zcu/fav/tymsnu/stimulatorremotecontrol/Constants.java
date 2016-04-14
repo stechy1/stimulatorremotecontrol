be07b2b775466494b4b6205899b0215d67080c97
Petr Štechmüller <stechy12@gmail.com>
2016-04-14 20:57:45 +0200

    #134 Přepsání logiky tlačítka zpět. Po zmáčknutí už nezůstane zaškrtnutá poslední nastavená položka v menu.

--- app/src/main/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/Constants.java
+++ app/src/main/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/Constants.java
@@ -11,11 +11,23 @@ public class Constants {
     public static final int MESSAGE_SHOW = 5;
 
     // Key names received from the BluetoothChatService Handler
-
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
 
     public static final int ERP_SCREEN_COUNT = 3;
+
+    // Konstanty představující jednotlivé fragmenty v aplikaci
+    public static final int FRAGMENT_ERP      =  0;
+    public static final int FRAGMENT_FVEP     =  1;
+    public static final int FRAGMENT_TVEP     =  2;
+    public static final int FRAGMENT_CVEP     =  3;
+    public static final int FRAGMENT_REA      =  4;
+    public static final int FRAGMENT_AUT      =  5;
+    public static final int FRAGMENT_BIO      =  6;
+    public static final int FRAGMENT_TEST     =  7;
+    public static final int FRAGMENT_SETTINGS =  8;
+    public static final int FRAGMENT_HELP     =  9;
+    public static final int FRAGMENT_ABOUT    = 10;
 
     public static final String FOLDER_ERP  = "erp";
     public static final String FOLDER_BCI  = "bci";
