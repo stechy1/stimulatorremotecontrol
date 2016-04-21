a2ee566884e7094908c256c157cb55fcd85c40d6
Petr Štechmüller <stechy12@gmail.com>
2016-04-21 13:39:08 +0200

    #145 Vytvoření spouštěcí třídy pro testy

---
+++ app/src/test/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/Test.java
@@ -0,0 +1,22 @@
+package cz.zcu.fav.tymsnu.stimulatorremotecontrol;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import cz.zcu.fav.tymsnu.stimulatorremotecontrol.model.AConfigurationTest;
+import cz.zcu.fav.tymsnu.stimulatorremotecontrol.model.ConfigurationERPTest;
+import cz.zcu.fav.tymsnu.stimulatorremotecontrol.utils.RangeUtilsParametrizedTest;
+import cz.zcu.fav.tymsnu.stimulatorremotecontrol.utils.RangeUtilsSingleTest;
+
+/**
+ * Hlavní testovací třída, spouští všechny ostatní testy
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AConfigurationTest.class,
+        ConfigurationERPTest.class,
+        RangeUtilsSingleTest.class,
+        RangeUtilsParametrizedTest.class
+})
+public class Test {
+}
