0e0b3e34e5303527b5097f9d27adfe166cf80376
Petr Štechmüller <stechy12@gmail.com>
2016-04-26 15:00:48 +0200

    #160 První otestování f-VEP konfigurace. Testujeme, zda-li jsou všechny položky na svém místě

---
+++ app/src/androidTest/java/cz/zcu/fav/tymsnu/stimulatorremotecontrol/fragments/bci/fvep/Screen2Test.java
@@ -0,0 +1,50 @@
+package cz.zcu.fav.tymsnu.stimulatorremotecontrol.fragments.bci.fvep;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cz.zcu.fav.tymsnu.stimulatorremotecontrol.MainActivity;
+import cz.zcu.fav.tymsnu.stimulatorremotecontrol.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static cz.zcu.fav.tymsnu.stimulatorremotecontrol.custom.action.NavigationViewActions.navigateTo;
+
+/**
+ * Testovací třída pro otestování GUI v f-VEPu 2. screenu
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class Screen2Test {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activity = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        // Otevřít postranní menu
+        openDrawer(R.id.activity_drawer_layout);
+        // Kliknout na položku f-VEP
+        onView(withId(R.id.nav_view)).perform(navigateTo(R.id.nav_item_2_1));
+        // Swipe na další screen
+        onView(withId(R.id.universal_view_pager)).perform(swipeLeft());
+    }
+
+    @Test
+    public void testCheckControls() throws Exception {
+        onView(withId(R.id.fvep_screen_2_swipe_number_picker)).check(matches(isDisplayed()));
+        onView(withId(R.id.universal_title)).check(matches(withText(R.string.screen_title_general_settings)));
+    }
+
+}
